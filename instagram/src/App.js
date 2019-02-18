6246e81cc7aaed09f93b33ac7dbbd41fbca9c258
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-02-18 18:47:25 +0000

    Add Search Bar Component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import fakeData from './dummy-data';
 import PostsContainer from './components/PostsContainer/PostsContainer.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import logo from './assets/ig-logo.png';
+import logoIcon from './assets/ig-logo-icon.svg';
 
 class App extends Component {
   constructor() {
@@ -9,12 +12,17 @@ class App extends Component {
       posts: fakeData
     };
   }
+
   render() {
     return (
       <div className="App">
         <header>
-          <div className="logo-container">Instagram</div>
-          {/*<SearchBar />*/}
+          <div className="logo-container">
+            <img src={logoIcon} alt="instagram logo"/>
+            <div className="seperator"></div>
+            <img src={logo} alt="instagram logo"/>
+          </div>
+          <SearchBar />
           <nav className="menu-container">
             <button>Explore</button>
             <button>Likes</button>
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import searchIcon from '../../assets/magnifying-glass.svg';
+
+class SearchBar extends Component {
+  render() {
+    return(
+      <div className="search-box">
+        <img src={searchIcon} alt="magnifying glass icon" />
+        <input type="text" placeholder="Search" />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
