21d54e234c26ede56b5ce5b79a6c972d038ff114
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-02-18 14:53:42 +0000

    Add React Project Structure

---
+++ instagram/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import fakeData from './dummy-data';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
