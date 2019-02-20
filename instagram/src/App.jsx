6b566c5aa04872bad371dc66e1219fcb61fe068a
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-02-20 15:53:36 +0000

    Create Login Page Component

--- instagram/src/App.jsx
+++ instagram/src/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import PostsPage from './components/PostsPage/PostsPage';
+import LoginPage from './components/LoginPage/LoginPage';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <PostsPage />
+      <LoginPage />
     );
   }
 }
---
+++ instagram/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  onInputChange = (event, fieldName) => {
+    // fieldName should correspond with the state key name
+    this.setState({ [fieldName]: event.target.value })
+  }
+
+  onLoginSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="login-page-container">
+        <form type="submit" onSubmit={e => this.onLoginSubmit(e)}>
+          <input
+            type="text"
+            value={this.state.username}
+            placeholder="your username please"
+            onChange={(e) => this.onInputChange(e, e.target.name)}
+            name="username"
+            maxLength="75"
+          />
+          <input
+            type="password"
+            value={this.state.password}
+            placeholder="your password"
+            onChange={(e) => this.onInputChange(e, e.target.name)}
+            name="password"
+          />
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
